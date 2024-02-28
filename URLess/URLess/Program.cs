5ae76208359914a3c1c1472271619b3f5f234f65
Nikolai Butkevich <da_nbutkevich@phreesia.com>
2024-02-28 09:35:41 +0200

    Added endpoints mocks

--- URLess/URLess/Program.cs
+++ URLess/URLess/Program.cs
@@ -1,6 +1,54 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/{url}", async (HttpContext context, string url) =>
+{
+    bool urlExists = false; 
+
+    if (urlExists)
+    {
+        context.Response.StatusCode = StatusCodes.Status301MovedPermanently;
+        context.Response.Headers.Add("Location", "https://example.com/new-url");
+    }
+    else
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+    }
+});
+
+app.MapPost("/", async (HttpContext context) =>
+{
+    using var reader = new StreamReader(context.Request.Body);
+    var requestBody = await reader.ReadToEndAsync();
+        
+    var jsonDocument = JsonDocument.Parse(requestBody);
+    var root = jsonDocument.RootElement;
+        
+    if (root.TryGetProperty("url", out var urlProperty))
+    {
+        string originalUrl = urlProperty.GetString();
+
+        string shortenedUrl = "handledValue";//ShortenUrl(originalUrl);
+
+        var responseJson = new
+        {
+            url = shortenedUrl,
+            originalURL = originalUrl
+        };
+
+        var jsonResponse = JsonSerializer.Serialize(responseJson);
+
+        context.Response.StatusCode = StatusCodes.Status201Created;
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync(jsonResponse);
+    }
+    else
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        await context.Response.WriteAsync("Bad Request: 'url' field is missing in the request body.");
+    }
+});
 
 app.Run();
